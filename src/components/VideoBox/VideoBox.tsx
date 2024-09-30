d77e2c417bc1707d9a47cd8e558fceaa6611668b
peltastic <onosogapelumi@gmail.com>
2024-09-30 16:05:47 +0100

    updates

--- src/components/VideoBox/VideoBox.tsx
+++ src/components/VideoBox/VideoBox.tsx
@@ -6,7 +6,7 @@ type Props = {};
 
 const VideoBox = (props: Props) => {
   return (
-    <div className="relative bg-[#00000080] h-[25.56rem]">
+    <div className="relative bg-[#00000080] h-[15rem] sm:h-[25.56rem]">
       <div className="absolute -translate-x-1/2 w-[8rem] h-[6.4rem] flex items-center justify-center rounded-md -translate-y-1/2 bg-[rgba(0,0,0,0.25)] left-1/2 top-1/2">
         <Image src={PlayButtonImage} alt="play-button" />
       </div>
