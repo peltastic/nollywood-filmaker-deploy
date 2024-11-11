dd3f6da4123a9beab8ba6dcd79379d853eae1e9b
peltastic <onosogapelumi@gmail.com>
2024-11-11 16:55:39 +0100

    updates

--- src/app/auth/reset-password/page.tsx
+++ src/app/auth/reset-password/page.tsx
@@ -1,3 +1,4 @@
+"use-client"
 import LeftSide from "@/components/Auth/LeftSide";
 import ResetPasswordForm from "@/components/Forms/Auth/ResetPasswordForm";
 import HomeLayout from "@/components/Layouts/HomeLayout";
