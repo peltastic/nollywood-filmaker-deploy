2d873d5ed55a198436fbded6e869a22d6e503098
PELUMI <63678094+peltastic@users.noreply.github.com>
2025-03-02 16:00:43 +0100

    Update Advert.tsx

--- src/components/Navbar/Advert.tsx
+++ src/components/Navbar/Advert.tsx
@@ -6,7 +6,7 @@ type Props = {};
 const Advert = (props: Props) => {
   return (
     <div className="bg-black-8 py-4">
-      <Link href={"https://www.youtube.com/"} target="_blank">
+      <Link href={"https://youtu.be/l9tNY0YxjQI?si=NsqPF7yXLvHrYABj"} target="_blank">
       <p className="text-sm text-center text-white text-[0.79rem]"><span className="text-yellow-1 hover:text-white hover:underline">Watch a tutorial on how our service works</span></p>
       </Link>
     </div>
